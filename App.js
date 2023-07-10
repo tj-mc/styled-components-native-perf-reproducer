1c0cefbb0a06fff5e043b2eb9a469a1caa6e3f9b
Tom McIntosh <tom_mcintosh@outlook.com>
2023-07-10 18:05:30 +1000

    initial commit

--- App.js
+++ App.js
@@ -1,14 +1,104 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Button, StyleSheet, Text, View} from 'react-native';
+import styled from "styled-components/native";
+import {useState} from "react";
 
-export default function App() {
+const NativeStyled = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.styledView}>
+        <View style={styles.styledView}>
+          <View style={styles.styledView}>
+            <View style={styles.styledView}>
+              <View style={styles.styledView}>
+                <View style={styles.styledView}>
+                  <View style={styles.styledView}>
+                    <View style={styles.styledView}>
+                      <View style={styles.styledView}>
+                        <View style={styles.styledView}>
+                          <View style={styles.styledView}>
+                            <View style={styles.styledView}>
+                              <View style={styles.styledView}>
+                                <View style={styles.styledView}>
+                                  <View style={styles.styledView}>
+                                  </View>
+                                </View>
+                              </View>
+                            </View>
+                          </View>
+                        </View>
+                      </View>
+                    </View>
+                  </View>
+                </View>
+              </View>
+            </View>
+          </View>
+        </View>
+      </View>
   );
 }
+
+const StyledComponentsView = () => {
+  return (
+    <StyledView>
+      <StyledView>
+        <StyledView>
+          <StyledView>
+            <StyledView>
+              <StyledView>
+                <StyledView>
+                  <StyledView>
+                    <StyledView>
+                      <StyledView>
+                        <StyledView>
+                          <StyledView>
+                            <StyledView>
+                              <StyledView>
+                                <StyledView>
+                                </StyledView>
+                              </StyledView>
+                            </StyledView>
+                          </StyledView>
+                        </StyledView>
+                      </StyledView>
+                    </StyledView>
+                  </StyledView>
+                </StyledView>
+              </StyledView>
+            </StyledView>
+          </StyledView>
+        </StyledView>
+      </StyledView>
+    </StyledView>
+  )
+}
+
+export default function App() {
+
+  const [version, setVersion] = useState('styled')
+
+  const onVersionPress = () => {
+    setVersion((v) => v === 'styled' ? 'native' : 'styled')
+  }
+
+  return (
+      <View style={styles.container}>
+        <Button title={`Switch to ${version === 'styled' ? 'native' : 'styled'}`} onPress={onVersionPress} />
+        <Text>Rendering with {
+            version === 'styled' ? 'styled-components' : 'native styles'
+        }</Text>
+        {
+            version === 'styled' ? <StyledComponentsView /> : <NativeStyled />
+        }
+      </View>
+  )
+}
+
+const StyledView = styled.View`
+  border-color: red;
+  border-width: 2px;
+  padding: 5px;
+`
 
 const styles = StyleSheet.create({
   container: {
@@ -17,4 +107,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  styledView: {
+    borderColor: "red",
+    borderWidth: 2,
+    padding: 5
+  }
 });
